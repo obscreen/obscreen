8801c13fc8af4887ed35a60864ea066932b2321b
JRK <jessym.reziga@gmail.com>
2024-10-18 16:50:37 +0200

    Update install-client-player.sh

--- system/client/install-client-player.sh
+++ system/client/install-client-player.sh
@@ -30,7 +30,7 @@ if [ "$disable_interaction" = false ]; then
         echo ""
         read -p "Enter Obscreen studio instance URL [default value: ${default_studio_url}]: " user_url
         obscreen_studio_url=${user_url:-$default_studio_url}
-        read -p "Do you confirm ${obscreen_studio_url} is a valid Obscreen studio instance? [Y/n]: " confirm
+        read -p "Do you confirm ${obscreen_studio_url} is a valid Obscreen studio instance? [y/N]: " confirm
         if [[ $confirm == "Y" || $confirm == "y" ]]; then
             echo ""
             echo "Using Obscreen studio instance URL: $obscreen_studio_url"
